1627adc817c521c6f47312267bb51132f404b66b
ScottVMariotte <ScottjMariotte@gmail.com>
2022-04-16 19:30:48 -0400

    Formatting.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,14 @@
 #
-#TODO: UI : Make a basic UI
+# TODO: UI : Make a basic UI
 #
 
+import bpy
 bl_info = {
-    "name" : "RigTools",
-    "blender": (3,0, 0),
-    "author" : "ScottVMariotte",
-    "version" : (0,0,1),
-    "category" : "Rigging"
+    "name": "RigTools",
+    "blender": (3, 0, 0),
+    "author": "ScottVMariotte",
+    "version": (0, 0, 1),
+    "category": "Rigging"
 }
 
 # Check if this add-on is being reloaded
@@ -20,26 +21,27 @@ else:
     importlib.reload(blender_ops)
     importlib.reload(tools)
 
-import bpy
 
-classes = { blender_ops.Gen_Bone_Curve,
-            blender_ops.Snap_Bones_to_Curve,
-            blender_ops.Gen_Constrain_Bones,
-            blender_ops.Gen_Eye_Bones,
-            blender_ops.Add_Twist_Constraints,
-            blender_ops.Remove_Constraints,
-            blender_ops.Add_Many_Constraints,
-            blender_ops.Toggle_Constraints,
-            blender_ops.Gen_Bone_Chain_From_Bones,
-            blender_ops.Gen_Bone_Copies,
-            blender_ops.Subdivide_Bones
-            }
+classes = {blender_ops.Gen_Bone_Curve,
+           blender_ops.Snap_Bones_to_Curve,
+           blender_ops.Gen_Constrain_Bones,
+           blender_ops.Gen_Eye_Bones,
+           blender_ops.Add_Twist_Constraints,
+           blender_ops.Remove_Constraints,
+           blender_ops.Add_Many_Constraints,
+           blender_ops.Toggle_Constraints,
+           blender_ops.Gen_Bone_Chain_From_Bones,
+           blender_ops.Gen_Bone_Copies,
+           blender_ops.Subdivide_Bones
+           }
+
 
 def register():
     #print("Registor classes")
     for c in classes:
         #print("Registoring - " + str(c))
         bpy.utils.register_class(c)
+
 
 def unregister():
     for c in classes:
