ac3a339e02cf5ca82d57c77d323630a413617c4b
ScottVMariotte <ScottjMariotte@gmail.com>
2022-06-15 12:03:18 -0400

    Added basic ui panel for displaying operations and Fixed import bug

--- __init__.py
+++ __init__.py
@@ -1,8 +1,3 @@
-#
-# TODO: UI : Make a basic UI
-#
-
-import bpy
 bl_info = {
     "name": "RigTools",
     "blender": (3, 0, 0),
@@ -11,35 +6,25 @@ bl_info = {
     "category": "Rigging"
 }
 
-# Check if this add-on is being reloaded
-if "bpy" not in locals():
-    from . import blender_ops
-    from . import tools
-else:
+if "bpy" in locals():
     import importlib
-
     importlib.reload(blender_ops)
     importlib.reload(tools)
+    importlib.reload(ui)
 
+else:
+    import bpy
+    from . import blender_ops
+    from . import tools
+    from . import ui
 
-classes = {blender_ops.Gen_Bone_Curve,
-           blender_ops.Snap_Bones_to_Curve,
-           blender_ops.Gen_Constrain_Bones,
-           blender_ops.Gen_Eye_Bones,
-           blender_ops.Add_Twist_Constraints,
-           blender_ops.Remove_Constraints,
-           blender_ops.Add_Many_Constraints,
-           blender_ops.Toggle_Constraints,
-           blender_ops.Gen_Bone_Chain_From_Bones,
-           blender_ops.Gen_Bone_Copies,
-           blender_ops.Subdivide_Bones
-           }
+classes = ui.classes.copy()
+classes.add(ui.RT_PT_RigTools)
+classes.add(ui.RigTPreferences)
 
 
 def register():
-    #print("Registor classes")
     for c in classes:
-        #print("Registoring - " + str(c))
         bpy.utils.register_class(c)
 
 
---
+++ ui.py
@@ -0,0 +1,42 @@
+import bpy
+from . import blender_ops
+
+classes = {blender_ops.Gen_Bone_Curve,
+           blender_ops.Snap_Bones_to_Curve,
+           blender_ops.Gen_Constrain_Bones,
+           blender_ops.Gen_Eye_Bones,
+           blender_ops.Add_Twist_Constraints,
+           blender_ops.Remove_Constraints,
+           blender_ops.Add_Many_Constraints,
+           blender_ops.Toggle_Constraints,
+           blender_ops.Gen_Bone_Chain_From_Bones,
+           blender_ops.Gen_Bone_Copies,
+           blender_ops.Subdivide_Bones,
+           blender_ops.Mod_Constraint_Space,
+           }
+
+
+class RT_PT_RigTools(bpy.types.Panel):
+    bl_idname = 'RT_PT_RigTools'
+    bl_label = 'RigTools Operations'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+
+    @ classmethod
+    def poll(cls, context):
+        return context.preferences.addons[__package__].preferences.panelToggle
+
+    def draw(self, context):
+        col = self.layout.column()
+        for operation in classes:
+            col.operator(operation.bl_idname, text=operation.bl_label)
+
+
+class RigTPreferences(bpy.types.AddonPreferences):
+    bl_idname = __package__
+
+    panelToggle: bpy.props.BoolProperty(name="Panel Toggle")
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "panelToggle")
